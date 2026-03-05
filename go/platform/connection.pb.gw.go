2e24c141f04fdabd123285e27d8b1a79bd7e1fe6
Karthi <karthi@clodpi.io>
2026-03-05 19:31:26 +0530

    feat: update ListConnectionGrid API endpoint and Swagger documentation

--- go/platform/connection.pb.gw.go
+++ go/platform/connection.pb.gw.go
@@ -1471,7 +1471,7 @@ var (
 
 	pattern_ConnectionService_DownloadReport_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"api", "connections", "bulk-upload", "report", "filename"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_ConnectionService_ListConnectionGrid_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "connections", "grid"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_ConnectionService_ListConnectionGrid_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"api", "connection-grid"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
