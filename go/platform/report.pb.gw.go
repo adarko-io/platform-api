2809c4fa904537221ae036fbff1d649752781ce0
Karthi <karthi@clodpi.io>
2026-03-19 13:07:13 +0530

    refactor: rename export job to report job and update related API and data structures
    - Changed ReportService methods to manage reports instead of export jobs.
    - Updated ListExportJobs to ListReports and DownloadExport to DownloadReport.
    - Modified ExportJob message to Report message with new fields for report metadata.
    - Introduced new enums for ReportType, ReportStatus, and ReportCategory.
    - Updated Swagger documentation to reflect changes in endpoints and data models.

--- go/platform/report.pb.gw.go
+++ go/platform/report.pb.gw.go
@@ -34,47 +34,47 @@ var _ = descriptor.ForMessage
 var _ = metadata.Join
 
 var (
-	filter_ReportService_ListExportJobs_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+	filter_ReportService_ListReports_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
 )
 
-func request_ReportService_ListExportJobs_0(ctx context.Context, marshaler runtime.Marshaler, client ReportServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq ListExportJobsRequest
+func request_ReportService_ListReports_0(ctx context.Context, marshaler runtime.Marshaler, client ReportServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq ListReportsRequest
 	var metadata runtime.ServerMetadata
 
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_ReportService_ListExportJobs_0); err != nil {
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_ReportService_ListReports_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := client.ListExportJobs(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.ListReports(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
 
-func local_request_ReportService_ListExportJobs_0(ctx context.Context, marshaler runtime.Marshaler, server ReportServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq ListExportJobsRequest
+func local_request_ReportService_ListReports_0(ctx context.Context, marshaler runtime.Marshaler, server ReportServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq ListReportsRequest
 	var metadata runtime.ServerMetadata
 
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_ReportService_ListExportJobs_0); err != nil {
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_ReportService_ListReports_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := server.ListExportJobs(ctx, &protoReq)
+	msg, err := server.ListReports(ctx, &protoReq)
 	return msg, metadata, err
 
 }
 
 var (
-	filter_ReportService_DownloadExport_0 = &utilities.DoubleArray{Encoding: map[string]int{"export_id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+	filter_ReportService_DownloadReport_0 = &utilities.DoubleArray{Encoding: map[string]int{"report_id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
 )
 
-func request_ReportService_DownloadExport_0(ctx context.Context, marshaler runtime.Marshaler, client ReportServiceClient, req *http.Request, pathParams map[string]string) (ReportService_DownloadExportClient, runtime.ServerMetadata, error) {
-	var protoReq DownloadExportRequest
+func request_ReportService_DownloadReport_0(ctx context.Context, marshaler runtime.Marshaler, client ReportServiceClient, req *http.Request, pathParams map[string]string) (ReportService_DownloadReportClient, runtime.ServerMetadata, error) {
+	var protoReq DownloadReportRequest
 	var metadata runtime.ServerMetadata
 
 	var (
@@ -84,25 +84,25 @@ func request_ReportService_DownloadExport_0(ctx context.Context, marshaler runti
 		_   = err
 	)
 
-	val, ok = pathParams["export_id"]
+	val, ok = pathParams["report_id"]
 	if !ok {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "export_id")
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "report_id")
 	}
 
-	protoReq.ExportId, err = runtime.String(val)
+	protoReq.ReportId, err = runtime.String(val)
 
 	if err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "export_id", err)
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "report_id", err)
 	}
 
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_ReportService_DownloadExport_0); err != nil {
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_ReportService_DownloadReport_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	stream, err := client.DownloadExport(ctx, &protoReq)
+	stream, err := client.DownloadReport(ctx, &protoReq)
 	if err != nil {
 		return nil, metadata, err
 	}
@@ -121,7 +121,7 @@ func request_ReportService_DownloadExport_0(ctx context.Context, marshaler runti
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterReportServiceHandlerFromEndpoint instead.
 func RegisterReportServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server ReportServiceServer) error {
 
-	mux.Handle("GET", pattern_ReportService_ListExportJobs_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_ReportService_ListReports_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
@@ -132,7 +132,7 @@ func RegisterReportServiceHandlerServer(ctx context.Context, mux *runtime.ServeM
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_ReportService_ListExportJobs_0(rctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_ReportService_ListReports_0(rctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -140,11 +140,11 @@ func RegisterReportServiceHandlerServer(ctx context.Context, mux *runtime.ServeM
 			return
 		}
 
-		forward_ReportService_ListExportJobs_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_ReportService_ListReports_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
-	mux.Handle("GET", pattern_ReportService_DownloadExport_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_ReportService_DownloadReport_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		err := status.Error(codes.Unimplemented, "streaming calls are not yet supported in the in-process transport")
 		_, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
@@ -192,7 +192,7 @@ func RegisterReportServiceHandler(ctx context.Context, mux *runtime.ServeMux, co
 // "ReportServiceClient" to call the correct interceptors.
 func RegisterReportServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client ReportServiceClient) error {
 
-	mux.Handle("GET", pattern_ReportService_ListExportJobs_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_ReportService_ListReports_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
@@ -201,18 +201,18 @@ func RegisterReportServiceHandlerClient(ctx context.Context, mux *runtime.ServeM
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_ReportService_ListExportJobs_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_ReportService_ListReports_0(rctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_ReportService_ListExportJobs_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_ReportService_ListReports_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
-	mux.Handle("GET", pattern_ReportService_DownloadExport_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_ReportService_DownloadReport_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
@@ -221,14 +221,14 @@ func RegisterReportServiceHandlerClient(ctx context.Context, mux *runtime.ServeM
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_ReportService_DownloadExport_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_ReportService_DownloadReport_0(rctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_ReportService_DownloadExport_0(ctx, mux, outboundMarshaler, w, req, func() (proto.Message, error) { return resp.Recv() }, mux.GetForwardResponseOptions()...)
+		forward_ReportService_DownloadReport_0(ctx, mux, outboundMarshaler, w, req, func() (proto.Message, error) { return resp.Recv() }, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -236,13 +236,13 @@ func RegisterReportServiceHandlerClient(ctx context.Context, mux *runtime.ServeM
 }
 
 var (
-	pattern_ReportService_ListExportJobs_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"api", "export-jobs"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_ReportService_ListReports_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"api", "reports"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_ReportService_DownloadExport_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"api", "export-jobs", "export_id", "download"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_ReportService_DownloadReport_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"api", "reports", "report_id", "download"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
-	forward_ReportService_ListExportJobs_0 = runtime.ForwardResponseMessage
+	forward_ReportService_ListReports_0 = runtime.ForwardResponseMessage
 
-	forward_ReportService_DownloadExport_0 = runtime.ForwardResponseStream
+	forward_ReportService_DownloadReport_0 = runtime.ForwardResponseStream
 )
